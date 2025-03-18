828e1361d715841cbcf337476ce69ed0bdb0d305
ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>
2025-03-18 14:19:49 +0000

    sm8250-common: Silence some spammy logging HWC and cit sensor logspam is so intense it bumps up logd's idle CPU usage up to over 3%. With this change, logs are much cleaner and logd idle CPU usage is reduced to under 1%, while still allowing error logs to pass through. - Do this only in user/debug builds (except eng). - Also build MatLog
    Change-Id: I4f84b4d96c8135d00f57e4502c2e1816be2eef55
    Signed-off-by: Sanjivns <113604406+Sanjivns@users.noreply.github.com>

--- kona.mk
+++ kona.mk
@@ -258,6 +258,25 @@ $(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
 $(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
 $(call soong_config_set_bool,lineage_health,charging_control_supports_bypass,false)
 
+# Logging
+ SPAMMY_LOG_TAGS := \
+     MiClstc \
+     MiStcImpl \
+     SDM \
+     SRE \
+     android.hardware.power-service-qti \
+     libsensor-boledalgo \
+     libsensor-parseRGB \
+     libsensor-qshcalapi \
+     sensors \
+     vendor.qti.hardware.display.composer-service \
+     vendor.xiaomi.sensor.citsensorservice-service.aidl
+
+ ifneq ($(TARGET_BUILD_VARIANT),eng)
+ PRODUCT_VENDOR_PROPERTIES += \
+     $(foreach tag,$(SPAMMY_LOG_TAGS),log.tag.$(tag)=E)
+ endif
+
 # Media configs
 PRODUCT_PACKAGES += \
     media_codecs_c2.xml \
